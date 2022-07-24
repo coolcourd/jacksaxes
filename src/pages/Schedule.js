5da0d277c1cb0b6d5b2f94ca069acce72908fa02
Courd Headman <cheadman@mediasolutionscorp.com>
2022-07-23 20:15:23 -0700

    added square preview

--- src/pages/Schedule.js
+++ src/pages/Schedule.js
@@ -6,13 +6,12 @@ import '../App.css';
 export default () => {
   return (
   <div className="App">
-  <header className="App-header">
     <p>
       SCHEDULE PAGE
     </p>
-    <iframe width='100%' height='750px' src='https://courd-courdh.zohobookings.com/portal-embed#/customer/4348918000000028071' frameborder='0' allowfullscreen='' > </iframe>  </header>
+    <iframe width='100%' height='750px' src='https://courd-courdh.zohobookings.com/portal-embed#/customer/4348918000000028071' frameborder='0' allowfullscreen='' > </iframe>
+    <iframe width='100%' height='750px' src='https://squareup.com/appointments/book/s49lod9rdhy0ea/L37888EYX08B1/start' frameborder='0' allowfullscreen='' > </iframe>
 
-    <script src='https://squareup.com/appointments/buyer/widget/j81an5osdx3n63/L37888EYX08B1.js'></script>
 </div>
 )
 }
